7526af2bb3cba8b4f181392043a0e9d151315019
Pawel Zapasnik <pzapasnik@gmail.com>
2025-02-19 18:45:36 +0100

    add basic http server

--- cmd/main.go
+++ cmd/main.go
@@ -4,22 +4,52 @@ import (
 	"context"
 	"log"
 	"log/slog"
+	"net/http"
 	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/pzapasnik/email-sender/internal/handler/index"
+	sloggin "github.com/samber/slog-gin"
+	"golang.org/x/sync/errgroup"
 )
 
 func main() {
+	if err := run(context.Background()); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func run(ctx context.Context) error {
 	logger := slog.New(slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{
 		Level: slog.LevelDebug,
 	}))
 
 	slog.SetDefault(logger)
 
-	if err := run(context.Background()); err != nil {
-		log.Fatal(err)
+	router := gin.New()
+	router.Use(sloggin.New(logger))
+	router.Static("/static", "web/static")
+
+	router.GET("/", index.New().Handle)
+
+	err := router.Run(":8080")
+	if err != nil {
+		return err
 	}
 
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: router,
+	}
+
+	errgrp, ctx := errgroup.WithContext(ctx)
+
+	errgrp.Go(func() error { return runServer(ctx, server) })
+
+	return nil
 }
 
-func run(ctx context.Context) error {
-	return nil
+func runServer(_ context.Context, server *http.Server) error {
+	//TODO: add graceful shutdown
+	return server.ListenAndServe()
 }
