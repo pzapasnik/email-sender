9c669044bec626bf24dfd447ba3cb567878915ff
Pawel Zapasnik <pzapasnik@gmail.com>
2025-03-15 13:23:57 +0100

    change web component

--- web/static/components/wysiwyg.js
+++ web/static/components/wysiwyg.js
@@ -1,23 +1,43 @@
 const template = document.createElement("template");
 template.innerHTML = `
 <style>
-  input {
-    width: 100%;
-    padding: 0.5rem;
-    margin-top: 0.5rem;
-    border: 1px solid #ccc;
-    border-radius: 0.25rem;
+  .container {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    padding: 2px;
+  }
+  .toolbar {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    padding: 2px;
+    border: 1px solid black;
+  }
+  .button {
+    padding: 2px;
+    margin: 2px;
+    border: 1px solid black;
+    border-radius: 4px;
+  }
+  .message {
+    height: 100%;
+  }
+  :host {
+    display: block;
+    height: 100%;
   }
 </style>
-<input type="textarea" placeholder="Type something..."/>
+<div class="container">
+  <slot name="label"></slot>
+  <slot name="toolbar"></slot>
+  <slot id="wysiwyg-editor-message"class="message" name="message"></slot>
+</div>
 `;
 
 class WYSIWYGEditor extends HTMLElement {
   constructor() {
     super();
-  }
-
-  connectedCallback() {
     const shadow = this.attachShadow({ mode: "open" });
     shadow.appendChild(template.content.cloneNode(true));
   }
