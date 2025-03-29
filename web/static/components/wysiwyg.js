386e4b36574bd98c2ebee0b95c5622bbbd1298c8
Pawel Zapasnik <pzapasnik@gmail.com>
2025-03-29 20:49:45 +0100

    add ritch text edditor icons

--- web/static/components/wysiwyg.js
+++ web/static/components/wysiwyg.js
@@ -1,3 +1,27 @@
+const boldIconPath = new URL("./icons/bold.svg", import.meta.url).href;
+const italicIconPath = new URL("./icons/italic.svg", import.meta.url).href;
+const underlinedIconPath = new URL("./icons/underlined.svg", import.meta.url)
+  .href;
+const stricThroughIconPath = new URL(
+  "./icons/stric_through.svg",
+  import.meta.url,
+).href;
+const alignLeftIconPath = new URL("./icons/align_left.svg", import.meta.url)
+  .href;
+const alignRightIconPath = new URL("./icons/align_right.svg", import.meta.url)
+  .href;
+const alignCenterIconPath = new URL("./icons/align_center.svg", import.meta.url)
+  .href;
+const alignJustifyIconPath = new URL(
+  "./icons/align_justify.svg",
+  import.meta.url,
+).href;
+const orderListIconPath = new URL("./icons/ordered_list.svg", import.meta.url);
+const unorderedListIconPath = new URL(
+  "./icons/unorderd_list.svg",
+  import.meta.url,
+);
+
 const template = document.createElement("template");
 template.innerHTML = `
 <style>
@@ -7,30 +31,94 @@ template.innerHTML = `
     flex-direction: column;
     padding: 2px;
   }
+
   .toolbar {
     display: flex;
     flex-direction: row;
-    justify-content: space-between;
-    padding: 2px;
-    border: 1px solid black;
+    position: relative;
   }
+
   .button {
     padding: 2px;
-    margin: 2px;
-    border: 1px solid black;
-    border-radius: 4px;
+    background-color: inherit;
+    border: 0px;
+
+    &:hover {
+      filter: brightness(0.8);
+      cursor: pointer;
+    }
+
   }
-  /* .message { */
-  /*   height: 100%; */
-  /* } */
-  /* :host { */
-  /*   display: block; */
-  /*   height: 100%; */
-  /* } */
+
+  .icon {
+    margin: auto auto;
+  }
+
+  .menu {
+    display: flex;
+    flex-direction: column;
+    z-index: 1;
+    background-color: inherit;
+    position: relative;
+
+    .submenu {
+      margin:   0px;
+      display: none;
+    }
+
+    &:hover {
+      .submenu {
+        display: block;
+        position: absolute;
+        top: 100%; 
+        left: 0; 
+        z-index: 10;
+        background-color: inherit;
+        border: 1px solid black;
+      }
+    }
+  }
 </style>
 <div class="container">
-  <slot name="label"></slot>
-  <slot name="toolbar"></slot>
+  <slot  name="label"></slot>
+  <div class="toolbar" part="toolbar">
+    <button part="button" class="button" data-action="bold" data-tag-name="b" title="Bold">
+      <img height="full" width="full" src="${boldIconPath}" alt="B"/>
+    </button>
+    <button part="button" class="button" data-action="italic" data-tag-name="i" title="Italic">
+      <img class="icon" src="${italicIconPath}" alt="I"/>
+    </button>
+    <button  part="button" class="button" data-action="underline" data-tag-name="u" title="Underline">
+      <img src="${underlinedIconPath}" alt="U"/>
+    </button>
+    <button  part="button" class="button" data-action="strikeThrough" data-tag-name="strike" title="Strike through">
+      <img src="${stricThroughIconPath}" alt="S"/>
+    </button>
+
+    <div class="menu">
+      <button class="button" part="button" data-action="justifyLeft" data-tag-name="div" title="Justify left">
+        <img part="icon" src="${alignLeftIconPath}" alt="Left"/>
+      </button>
+      <div class="submenu">
+        <button class="button" part="button" data-action="justifyCenter" data-tag-name="div" title="Justify center">
+          <img src="${alignCenterIconPath}" alt="Center"/>
+        </button>
+        <button class="button" part="button" data-action="justifyRight" data-tag-name="div" title="Justify right">
+          <img src="${alignRightIconPath}" alt="Right"/>
+        </button>
+        <button class="button" part="button" data-action="justifyFull" data-tag-name="div" title="Justify full">
+          <img src="${alignJustifyIconPath}" alt="Full"/>
+        </button>
+      </div>
+    </div>
+
+    <button part="button" class="button" data-action="insertOrderedList" data-tag-name="ol" title="Ordered list">
+      <img src="${orderListIconPath}" alt="OL"/>
+    </button>
+    <button part="button" class="button" data-action="insertUnorderedList" data-tag-name="ul" title="Unordered list">
+      <img src="${unorderedListIconPath}" alt="UL"/>
+    </button>
+  </div>
   <slot id="wysiwyg-editor-message"class="message" name="message"></slot>
 </div>
 `;
